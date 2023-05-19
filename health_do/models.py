b60ce332921230ac62d1729ecee1528e9578b9d2
RyunK <lrk9582@gmail.com>
2023-05-19 19:31:50 +0900

    first commit

---
+++ health_do/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from main.models import User_data
+
+class Train_video(models.Model):
+    video_name = models.CharField(max_length=20)
+    video_id = models.CharField(max_length=20, primary_key=True)
+    main_body = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.video_id
+
+class Training_data(models.Model):
+    user_id = models.ForeignKey(User_data, on_delete=models.CASCADE)
+    video_id = models.ForeignKey(Train_video, on_delete=models.CASCADE)
+    Rarm = models.FloatField()
+    Larm = models.FloatField()
+    Relbow = models.FloatField()
+    Lelbow = models.FloatField()
+    Rwaist = models.FloatField()
+    Lwaist = models.FloatField()
+    Rleg = models.FloatField()
+    Lleg = models.FloatField()
+    Rknee = models.FloatField()
+    Lknee = models.FloatField()
+
+    def __str__(self):
+        return self.user_id
